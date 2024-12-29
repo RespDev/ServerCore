48b98c70e07d300e05162503e5fc6cf946ed410d
RespDev <mail.nathan.flowers@gmail.com>
2024-12-29 08:44:53 -0600

    A few utilities.

--- src/main/java/codes/settlement/core/Core.java
+++ src/main/java/codes/settlement/core/Core.java
@@ -1,17 +1,37 @@
 package codes.settlement.core;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
     public static Core instance;
 
+    @Getter @Setter public String tabHeader = "";
+    @Getter @Setter public String tabFooter = "";
+
     @Override
-    public void onEnable(){
+    public void onEnable() {
         instance = this;
+
+        // Load config utility
+        // soon
+
+        // Load tab from config
+        tabHeader = ChatColor.translateAlternateColorCodes('&', "&b&lTEST NETWORK");
+        tabFooter = ChatColor.translateAlternateColorCodes('&', "&aplay.test.com");
+
+        // Register listeners
+        registerListeners();
     }
 
     @Override
-    public void onDisable(){
+    public void onDisable() {
+
+    }
+
+    private void registerListeners() {
 
     }
 
---
+++ src/main/java/codes/settlement/core/util/TabUtil.java
@@ -0,0 +1,29 @@
+package codes.settlement.core.util;
+
+import codes.settlement.core.Core;
+import net.minecraft.server.v1_8_R3.ChatComponentText;
+import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerListHeaderFooter;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import java.lang.reflect.Field;
+
+public class TabUtil {
+    public void setHeaderFooter(Player player) {
+		PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
+
+		try {
+			Field a = packet.getClass().getDeclaredField("a");
+			a.setAccessible(true);
+
+			Field b = packet.getClass().getDeclaredField("b");
+			b.setAccessible(true);
+
+			a.set(packet, new ChatComponentText(Core.getInstance().getTabHeader()));
+			b.set(packet, new ChatComponentText(Core.getInstance().getTabFooter()));
+
+			((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
