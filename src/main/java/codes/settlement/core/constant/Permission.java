0bda3c821b8ddd178806954007f3fa40b21b3455
RespDev <93840464+RespDev@users.noreply.github.com>
2025-05-15 21:37:50 -0500

    Added all the ranks to the Permission Constant and updated the gitignore.

--- src/main/java/codes/settlement/core/constant/Permission.java
+++ src/main/java/codes/settlement/core/constant/Permission.java
@@ -1,16 +1,29 @@
 package codes.settlement.core.constant;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 public final class Permission {
 
     /*
      * Rank permissions for the Core Plugin
      */
     public static final String DIRECTOR = "galaxyparks.director";
+    public static final String MANAGER = "galaxyparks.manager";
+    public static final String ADMIN = "galaxyparks.admin";
     public static final String DEVELOPER = "galaxyparks.developer";
+    public static final String COORDINATOR = "galaxyparks.coordinator";
+    public static final String ARCHITECT = "galaxyparks.architect";
     public static final String BUILDER = "galaxyparks.builder";
     public static final String MOD = "galaxyparks.mod";
-    public static final String STAFF = "galaxyparks.staff";
+    public static final String TRAINEE = "galaxyparks.trainee";
+    public static final String STAFF = "galaxyparks.staff"; // General staff permission that applies to Trainee+
+    public static final String CHARACTER = "galaxyparks.character";
     public static final String SPECIALGUEST = "galaxyparks.specialguest";
+    public static final String SHAREHOLDER = "galaxyparks.shareholder";
+    public static final String HONORABLE = "galaxyparks.honorable";
+    public static final String MAJESTIC = "galaxyparks.majestic";
+    public static final String NOBLE = "galaxyparks.noble";
+    public static final String DWELLER = "galaxyparks.dweller";
     public static final String SETTLER = "galaxyparks.settler";
 
     /*
