36be521b310c87897aad3c161f7d825e5a02ce5f
RespDev <93840464+RespDev@users.noreply.github.com>
2025-05-16 22:36:48 -0500

    Update MenuListener.java

--- src/main/java/codes/settlement/core/listener/MenuListener.java
+++ src/main/java/codes/settlement/core/listener/MenuListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MenuListener implements Listener {
@@ -66,5 +67,4 @@ public class MenuListener implements Listener {
             }
         }
     }
-
 }
